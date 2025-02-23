91085ca41baea8148e7b9c6f6ee0e6281599da73
harlanwade <krystof.karel@gmail.com>
2025-02-23 17:35:12 +0100

    Update 4) Payroll and prices increase difference.sql

--- sql/4) Payroll and prices increase difference.sql
+++ sql/4) Payroll and prices increase difference.sql
@@ -1,10 +1,13 @@
+-- Vytváření dočasné tabulky 'value_changes', která obsahuje změny mezd a cen pro jednotlivé roky
 with value_changes as (
+
+	-- Výběr údajů pro mzdy, včetně předchozích hodnot
 	select
 	    year,
 	    industry_branch_name,
 	    null as category_name,
 	    payroll_value,
-	    lag(payroll_value) over (
+	    lag(payroll_value) over (  -- Získání předchozí hodnoty mzdy pro dané odvětví
 	        partition by industry_branch_name 
 	        order by year
 	    ) as prev_payroll_value,
@@ -13,7 +16,9 @@ with value_changes as (
 	from t_krystof_karel_project_SQL_primary_final
 		where industry_branch_name is not null
 
-	union all
+	union all  -- Spojení s dalším výběrem pro ceny (bez mezd)
+
+	-- Výběr údajů pro ceny, včetně předchozích hodnot
 
 	select
 	    year,
@@ -22,30 +27,31 @@ with value_changes as (
 	    null as payroll_value,
 	    null as prev_payroll_value,
 	    price_value,
-	    lag(price_value) over (
+	    lag(price_value) over (  -- Získání předchozí hodnoty ceny pro danou kategorii
 	        partition by category_name 
 	        order by year
 	    ) as prev_price_value
 	from t_krystof_karel_project_SQL_primary_final
 		where category_name is not null
 	)
+-- Hlavní dotaz pro výpočet průměrné změny mezd, průměrné změny cen a jejich rozdílu
 select
 	year,
-    round(avg(
+    round(avg(  -- Výpočet průměrné procentuální změny mezd
         case 
             when prev_payroll_value is not null then 
                 ((payroll_value - prev_payroll_value) / prev_payroll_value) * 100
             else null
         end
     ), 2) as avg_payroll_increase_percent,
-    round(avg(
+    round(avg(  -- Výpočet průměrné procentuální změny cen
         case 
             when prev_price_value is not null then 
                 ((price_value - prev_price_value) / prev_price_value) * 100
             else null
         end
     ), 2) as avg_price_increase_percent,
-    (round(avg(
+    (round(avg(  -- Výpočet rozdílu mezi průměrnou změnou cen a průměrnou změnou mezd
         case 
             when prev_price_value is not null then 
                 ((price_value - prev_price_value) / prev_price_value) * 100
@@ -53,13 +59,13 @@ select
         end
     ), 2))
     -
-    (round(avg(
+    (round(avg(  -- Odečítání průměrné změny mezd
         case 
             when prev_payroll_value is not null then 
                 ((payroll_value - prev_payroll_value) / prev_payroll_value) * 100
             else null
         end
-    ), 2)) as difference
+    ), 2)) as difference  -- Výsledek je rozdíl mezi průměrnými změnami cen a mezd
 from value_changes
 group by year
 order by year;
