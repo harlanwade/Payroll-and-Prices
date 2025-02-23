90a6be57bce57a1680f455ca9e048f1e920d72cf
harlanwade <krystof.karel@gmail.com>
2025-02-23 17:25:18 +0100

    Update Table 1 – CZE.sql

--- sql/Table 1 – CZE.sql
+++ sql/Table 1 – CZE.sql
@@ -5,38 +5,38 @@ create table t_krystof_karel_project_SQL_primary_final as
 
 -- Průměrné mzdy podle odvětví
 select
-    cpay.payroll_year as year,  -- Rok
-    cpib.name as industry_branch_name,  -- Název odvětví
-    round(avg(cpay.value)::numeric, 2) as payroll_value,  -- Průměrná mzda
+    cpay.payroll_year as year,
+    cpib.name as industry_branch_name,
+    round(avg(cpay.value)::numeric, 2) as payroll_value,
     NULL as category_name,
     NULL as price_value
 from czechia_payroll cpay 
 join czechia_payroll_industry_branch cpib 
-    on cpay.industry_branch_code = cpib.code  -- Spojení s odvětvím
-where cpay.value_type_code = 5958  -- Filtr na mzdy
+    on cpay.industry_branch_code = cpib.code
+where cpay.value_type_code = 5958
 group by
-    cpay.payroll_year,  -- Seskupení podle roku
-    cpay.industry_branch_code,  -- Seskupení podle kódu odvětví
-    cpib.name  -- Seskupení podle názvu odvětví
+    cpay.payroll_year,
+    cpay.industry_branch_code,
+    cpib.name
 
 union all
 
 -- Průměrné ceny podle kategorií
 select
-    cpay.payroll_year as year,  -- Rok
+    cpay.payroll_year as year,
     NULL as industry_branch_name,
     NULL as payroll_value,
-    cpc.name as category_name,  -- Název kategorie
-    round(avg(cpr.value)::numeric, 2) as price_value  -- Průměrná cena
+    cpc.name as category_name,
+    round(avg(cpr.value)::numeric, 2) as price_value
 from czechia_price cpr 
 join czechia_price_category cpc 
-    on cpr.category_code = cpc.code  -- Spojení s kategorií
+    on cpr.category_code = cpc.code
 join czechia_payroll cpay
-    on cpay.payroll_year = date_part('year', cpr.date_from)  -- Spojení s rokem výplaty
-where cpay.value_type_code = 5958  -- Filtr na mzdy
+    on cpay.payroll_year = date_part('year', cpr.date_from)
+where cpay.value_type_code = 5958
 group by
-    cpay.payroll_year,  -- Seskupení podle roku
-    cpc.name  -- Seskupení podle názvu kategorie
+    cpay.payroll_year,
+    cpc.name
 
 -- Seřazení výsledků podle roku, odvětví a kategorie
 order by year, industry_branch_name, category_name;
