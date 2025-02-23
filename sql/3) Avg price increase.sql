de5f32027b08b5b6877c257dc96e6758dbc82690
harlanwade <krystof.karel@gmail.com>
2025-02-23 17:30:01 +0100

    Update 3) Avg price increase.sql

--- sql/3) Avg price increase.sql
+++ sql/3) Avg price increase.sql
@@ -1,17 +1,22 @@
+-- Vytváření dočasné tabulky, která obsahuje aktuální a předcházející ceny pro každou kategorii
+
 with price_changes as (
     select 
         category_name,
         year,
         price_value,
-        lag(price_value) over (
+        lag(price_value) over (  -- Získání předchozí ceny pro každou kategorii
             partition by category_name 
             order by year
         ) as prev_price_value
     from t_krystof_karel_project_SQL_primary_final
 )
+
+-- Hlavní dotaz pro výpočet průměrného procentuálního nárůstu ceny mezi roky pro každou kategorii
+
 select 
     category_name,
-    round(avg(
+    round(avg(  -- Výpočet průměrného procentuálního nárůstu ceny
         case 
             when prev_price_value is not null then 
                 ((price_value - prev_price_value) / prev_price_value) * 100
