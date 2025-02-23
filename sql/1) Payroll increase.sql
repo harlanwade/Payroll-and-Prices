7d0d5e64ebd71b157f70e566efe783816d42271d
harlanwade <krystof.karel@gmail.com>
2025-02-23 17:04:13 +0100

    Update and rename 1) Payroll increase to 1) Payroll increase.sql

--- sql/1) Payroll increase.sql
+++ sql/1) Payroll increase.sql
@@ -1,29 +1,26 @@
 select
 	year,
-	quarter,
 	industry_branch_name,
 	payroll_value,
 	lag(payroll_value) over (
-        partition by industry_branch_name, quarter
+        partition by industry_branch_name
         order by year
 	    ) as previous_year_value,
     case 
         when lag(payroll_value) over (
-            partition by industry_branch_name, quarter
+            partition by industry_branch_name
             order by year
         ) is null then 'N/A'
         when payroll_value > lag(payroll_value) over (
-            partition by industry_branch_name, quarter
+            partition by industry_branch_name
             order by year
         ) then 'Higher'
         when payroll_value < lag(payroll_value) over (
-            partition by industry_branch_name, quarter
+            partition by industry_branch_name
             order by year
         ) then 'Lower'
         else 'Same'
     end as comparison_to_previous_year
 from t_krystof_karel_project_SQL_primary_final
-where
-	quarter = 1
-	and industry_branch_name is not null
-order by industry_branch_name, quarter, year;
+where industry_branch_name is not null
+order by industry_branch_name, year;
