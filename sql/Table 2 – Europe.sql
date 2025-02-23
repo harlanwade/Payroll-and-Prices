2672f2f0d9059a56035910565c2a54dee624a12d
harlanwade <krystof.karel@gmail.com>
2025-02-23 17:17:00 +0100

    Update Table 2 – Europe.sql

--- sql/Table 2 – Europe.sql
+++ sql/Table 2 – Europe.sql
@@ -1,15 +1,19 @@
+-- Vytváříme tabulku s ekonomickými údaji pro evropské země (kromě České republiky)
+-- ve sledovaném období
+
 create table t_krystof_karel_project_SQL_secondary_final as
 select
-	country,
-	year,
-	gdp,
-	gini,
-	population
+    country,
+    year,
+    gdp,
+    gini,
+    population
 from economies e
 where country in (
-	select country
-	from countries c 
-	where region_in_world like '%Europe'
-	)
+    -- Výběr zemí z Evropy
+    select country
+    from countries c 
+    where region_in_world like '%Europe'
+    )
 and country != 'Czech Republic'
 and year >= 2000
